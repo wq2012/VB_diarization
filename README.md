f2bed16bb216c80435296a75a5c077f10129fd3f
Quan Wang <quanw@google.com>
2019-02-06 23:13:17 -0500

    Add README.md and vscode config

---
+++ README.md
@@ -0,0 +1,8 @@
+# VB Diarization with Eigenvoice and HMM Priors
+
+## Overview
+
+This is a **refactored** version of the VB diarization software available at
+[BUT Speech@FIT](https://speech.fit.vutbr.cz/software/vb-diarization-eigenvoice-and-hmm-priors).
+
+I am **NOT** the original author of the library.
--- VB_diarization/VB_diarization_demo.py
+++ VB_diarization/VB_diarization_demo.py
@@ -22,16 +22,16 @@ import numpy as np
 import VB_diarization
 
 # read UBM and iXtractor (total variability) subspace
-m  = np.loadtxt("UBM_means.txt", dtype=np.float32)
+m = np.loadtxt("UBM_means.txt", dtype=np.float32)
 iE = 1.0 / np.loadtxt("UBM_vars.txt", dtype=np.float32)
-w  = np.loadtxt("UBM_weights.txt", dtype=np.float32)
-V  = np.loadtxt("iXtractor.txt", dtype=np.float32).reshape(-1,*m.shape)
+w = np.loadtxt("UBM_weights.txt", dtype=np.float32)
+V = np.loadtxt("iXtractor.txt", dtype=np.float32).reshape(-1, *m.shape)
 
 # load MFCC features
-X=np.loadtxt("features.txt")
+X = np.loadtxt("features.txt")
 
-#load reference per-frame labels (0: silence; 1: both; 2: speaker; A 3: speaker B)
-ref=np.loadtxt("ref.txt", dtype=int)
+# load reference per-frame labels (0: silence; 1: both; 2: speaker; A 3: speaker B)
+ref = np.loadtxt("ref.txt", dtype=int)
 
 # keep only frames with one speaker (to make scoring simple)
 X = X[ref > 1]
@@ -40,14 +40,14 @@ ref = ref[ref > 1] - 2
 VtiEV = VB_diarization.precalculate_VtiEV(V, iE)
 
 q = None
-#q = VB_diarization.frame_labels2posterior_mx(ref) # initialize from the reference
+# q = VB_diarization.frame_labels2posterior_mx(ref) # initialize from the reference
 
 # runing with one 25 frame (0.25s) resolution; about 2.5 faster
 q, sp, L = VB_diarization.VB_diarization(X, m, iE, w, V, sp=None, q=None, maxSpeakers=2, maxIters=10, VtiEV=VtiEV,
-                                  downsample=25, alphaQInit=100.0, sparsityThr=0.001, epsilon=1e-6, minDur=1,
-                                  loopProb=0.9, statScale=0.3, llScale=1.0, ref=ref, plot=False)
+                                         downsample=25, alphaQInit=100.0, sparsityThr=0.001, epsilon=1e-6, minDur=1,
+                                         loopProb=0.9, statScale=0.3, llScale=1.0, ref=ref, plot=False)
 
 # runing with one frame resolution
 q, sp, L = VB_diarization.VB_diarization(X, m, iE, w, V, sp=None, q=None, maxSpeakers=2, maxIters=10, VtiEV=VtiEV,
-                                  downsample=1, alphaQInit=100.0, sparsityThr=0.001, epsilon=1e-6, minDur=1,
-                                  loopProb=0.998, statScale=0.3, llScale=1.0, ref=ref, plot=True)
+                                         downsample=1, alphaQInit=100.0, sparsityThr=0.001, epsilon=1e-6, minDur=1,
+                                         loopProb=0.998, statScale=0.3, llScale=1.0, ref=ref, plot=True)
